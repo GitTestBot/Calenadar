cc9247e0696f9d089b27801e0beaa715f1e3be1d
Do <Do@joon>
2018-10-02 22:55:30 +0900

    Sum class push

---
+++ src/calendar/Sum.java
@@ -0,0 +1,13 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+		System.out.println(x+y);
+	}
+}
