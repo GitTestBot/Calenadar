b79ebc7fb8d03642f9cb025efc08aeb1a5543dbc
Do <Do@joon>
2018-10-03 13:29:01 +0900

    Update find last_day

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println("일 	월 	화 	수 	목 	금 	토");
@@ -8,5 +10,18 @@ public class Calendar {
 		System.out.println("8	9	10	11	12	13	14");
 		System.out.println("15	16	17	18	19	20	21");
 		System.out.println("22	23	24	25	26	27	28");
+		
+		Scanner scan = new Scanner(System.in);
+		int[] last_day = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.println("달을 입력하세요.");
+		
+		int num = scan.nextInt();
+		if(1<= num && num<= 12) {
+			System.out.println(num+"월은 "+last_day[num-1]+"까지 있습니다.");
+		}
+		
+		
+		
+		
 	}
 }
